c8a8024f648426fae4fb080e325e9fbebc2deb75
pratalytics <50282777+pratalytics@users.noreply.github.com>
2019-08-06 07:54:12 +0530

    Update displayValues.js

--- users/01/displayValues.js
+++ users/01/displayValues.js
@@ -3,3 +3,10 @@
       entitySheet.getRange(char + (index + 2)).setValue(el)
     })
   }
+  
+ 
+  var displayOverallQAResults = function (entityArr, entitySheet, char) {
+    entityArr.forEach(function (el, index) {
+      entitySheet.getRange(char + (index + 2)).setValue(JSON.stringify(el))
+    })
+  }
