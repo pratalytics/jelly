af1b82a3b3652b623027c93d59d5e88184c8b3df
pratalytics <50282777+pratalytics@users.noreply.github.com>
2019-08-17 16:49:49 +0530

    Update code.js

--- users/01/code.js
+++ users/01/code.js
@@ -1,22 +1,24 @@
 function myFunction() {
   
-  var workSpace = TagManager.Accounts.Containers.Workspaces.getStatus(path)
-  overViewSheet.getRange('B11').setValue(JSON.stringify(workSpace))
-  var wsChangesArr = workSpace.workspaceChange
-  overViewSheet.getRange('B13').setValue(JSON.stringify(wsChangesArr))
+  
+  sheets.overview.getRange('B11').setValue(JSON.stringify(workSpace))
+  
+  sheets.overview.getRange('B13').setValue(JSON.stringify(wsChangesArr))
+  
+  cleanSheets(sheets)
 
-  tagSheet.deleteRows(2, tagSheet.getLastRow())
-  variableSheet.deleteRows(2, variableSheet.getLastRow())
-  triggerSheet.deleteRows(2, triggerSheet.getLastRow())
   
-  gtmResource(wsChangesArr,'tag',tagArr)
-  gtmResource(wsChangesArr,'variable',variableArr)
-  gtmResource(wsChangesArr,'trigger',triggerArr)
+
+  gtmResource(wsChangesArr, gtmResources)
+  sheets.overview.getRange('B15').setValue(JSON.stringify(gtmResources))
   
   
-  tagResource(tagArr, 'name', tagNameArr)
-  variableResource(variableArr, 'name', variableNameArr)
-  triggerResource(triggerArr, 'name', triggerNameArr)
+  
+  displayGTMResources(gtmResources, sheets, "B", "name")
+  displayGTMResources(gtmResources, sheets, "D", "changeStatus")
+  
+  /*
+  
   
   tagQAFun(tagNameArr, tagQATests, tagQAResults)
   //Logger.log(tagQAResults)
@@ -24,15 +26,14 @@ function myFunction() {
   orderedQAFun(tagNameArr,tagQAResults,orderedTagQAResults)
   //Logger.log(orderedTagQAResults[0])
   
-  displayEntityNames(tagNameArr,tagSheet, "B")
-  displayEntityNames(variableNameArr,variableSheet, "B")
-  displayEntityNames(triggerNameArr,triggerSheet, "B")
+
   displayEntityNames(tagArr,tagSheet, "G")
   displayOverallQAResults(orderedTagQAResults,tagSheet, "H")
   //displayEntityNames(triggerNameArr,triggerSheet, "B")
-  //displayEntityNames(variableNameArr,variabaleSheet, "B")
+  //displayEntityNames(variableNameArr,variabaleSheet, "B")*/
   
-  tagSheet.autoResizeColumn(2)
+
+  autoResizeCols(sheets)
   
  
 }
