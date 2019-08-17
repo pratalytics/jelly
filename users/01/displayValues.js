daa2ef92bbceb82d8ecc6a44d566425ccaaea920
pratalytics <50282777+pratalytics@users.noreply.github.com>
2019-08-17 16:51:18 +0530

    Update displayValues.js

--- users/01/displayValues.js
+++ users/01/displayValues.js
@@ -10,3 +10,29 @@
       entitySheet.getRange(char + (index + 2)).setValue(JSON.stringify(el))
     })
   }
+  
+  var cleanSheets = function (sheetObj) {
+    for (var key in sheetObj) {
+        if (key !== 'overview') {
+            sheetObj[key].deleteRows(2, sheetObj[key].getLastRow())
+        }
+    }
+}
+  
+
+var displayGTMResources = function (gtmResources, sheets, char, key) {
+    for (var prop in gtmResources) {
+        if (gtmResources[prop].length > 0) {
+            gtmResources[prop].forEach( function (el, index) {
+                sheets[prop].getRange(char + (index + 2)).setValue(el[key])
+            })
+        }
+    }
+}
+
+
+var autoResizeCols = function (sheets) {
+    for (var prop in sheets) {
+        sheets[prop].autoResizeColumns(2,2)
+    }
+}
