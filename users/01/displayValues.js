9dabec81b3fac31897a540e305b202902158ef4e
pratalytics <50282777+pratalytics@users.noreply.github.com>
2019-08-20 14:29:51 +0530

    Update displayValues.js

--- users/01/displayValues.js
+++ users/01/displayValues.js
@@ -1,9 +1,3 @@
-  var displayEntityNames = function (entityArr, entitySheet, char) {
-    entityArr.forEach(function (el, index) {
-      entitySheet.getRange(char + (index + 2)).setValue(el)
-    })
-  }
-  
  
   var displayOverallQAResults = function (entityArr, entitySheet, char) {
     entityArr.forEach(function (el, index) {
@@ -33,6 +27,43 @@ var displayGTMResources = function (gtmResources, sheets, char, key) {
 
 var autoResizeCols = function (sheets) {
     for (var prop in sheets) {
-        sheets[prop].autoResizeColumns(2,2)
+        sheets[prop].autoResizeColumns(2,3)
     }
+}
+
+
+
+var displayGAEvents = function (gtmResources, sheets) {
+    
+    var vals = []
+    gtmResources.tag.forEach(function (el) {
+      var tag = {
+        'name': '',
+        'eventCategory': '',
+        'eventAction': '',
+        'eventLabel': '',
+        'nonInteraction': ''
+    }
+      
+        el.parameter.forEach(function (pel) {
+            if (pel.key === 'eventCategory') {
+                tag.name = el.name
+                tag.eventCategory = pel.value
+            } else if (pel.key === 'eventAction') {
+                tag.eventAction = pel.value
+            } else if (pel.key === 'eventLabel') {
+                tag.eventLabel = pel.value
+            } else if (pel.key === 'nonInteraction') {
+                tag.nonInteraction = pel.value
+            }
+            
+        })
+        if (tag.name != '') {
+          vals.push([tag.name, tag.eventCategory, tag.eventAction, tag.eventLabel, tag.nonInteraction])
+        }
+        
+        
+
+    })
+    sheets['events'].getRange('B2:F' + (vals.length+1)).setValues(vals)
 }
